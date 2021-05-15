5ad05a2a85579f02db34f38ea96c1edc4f44e734
spmvg <13852721+spmvg@users.noreply.github.com>
2021-05-15 14:06:08 +0200

    add link to nfoursid

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,6 +7,7 @@
           <b-navbar-nav class="ml-auto">
             <b-nav-item to="/" class="mx-1">HOME</b-nav-item>
             <b-nav-item to="/blog" class="mx-1">BLOG</b-nav-item>
+            <b-nav-item href="https://pypi.org/project/nfoursid/" class="mx-1" target="_blank">NFOURSID</b-nav-item>
             <b-nav-item to="/about" class="mx-1">ABOUT</b-nav-item>
             <b-nav-item href="https://github.com/spmvg" class="mx-1" target="_blank">GITHUB</b-nav-item>
             <b-nav-item href="https://www.linkedin.com/in/spmvangemert/" class="mx-1" target="_blank">LINKEDIN</b-nav-item>
