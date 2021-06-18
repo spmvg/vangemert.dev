d7be0653add373a5b89afc286219079c13dea92b
spmvg <13852721+spmvg@users.noreply.github.com>
2021-06-18 19:27:23 +0200

    write a short introduction for the homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="md my-3">
     <b-row class="my-2 py-2 mx-2" align-h="center">
         <b-col cols="12" lg="7">
-            <p>{{short_description}}</p>
+            <p>{{intro}}<br><br>{{short_description}}</p>
         </b-col>
         <b-col cols="5">
             <img src="images/steven_van_gemert.png">
@@ -12,11 +12,13 @@
 </template>
 
 <script>
+import intro from '@/components/intro.js'
 import short_description from '@/components/description.js'
 
 export default {
   data() {
     return {
+      intro: intro,
       short_description: short_description
     }
   }
--- src/components/description.js
+++ src/components/description.js
@@ -1,4 +1,5 @@
 // Make a short description available for multiple views
-const short_description = 'Data scientist, python developer, physicist. Passions include guitar, home automation and fat-tailed probability theory.'
+const short_description =  'I am a data scientist, Python developer and physicist. '+
+    'My passions include guitar, home automation and fat-tailed probability theory.'
 
 export default short_description
